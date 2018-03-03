59fbcfb911f87b5c959fd4b8229b7f0b8df6568a
Sindre Gjerde <sindread@stud.ntnu.no>
2018-03-03 14:14:37 +0100

    fikse alt

--- P1/src/zeta1/main.cpp
+++ P1/src/zeta1/main.cpp
@@ -3,27 +3,37 @@
 #include <iostream>
 #include <sstream>
 #include <limits>
+#include <mpi.h>
 
 #define M_PI acos(-1.0)
 
 using namespace std;
 
+double calculate_pi(int &rank, const int &nop, const int &n)
+{
+	return zeta1_calculate_pi(rank , nop, n);
+}
 
-bool unit_test()
+bool unit_test(int &rank)
 {
 	const auto answer_should_be = &zeta1_expected_value_after_3_iterations;
 
-	return *answer_should_be == run_calculation(2, 3);
+	return *answer_should_be == calculate_pi(rank, 2, 3);
 }
 
-string verification_test(const int maxk)
+string verification_test(int &rank, const int maxk)
 {
 	ostringstream oss;
 	auto n = 0;
 	for (auto k = 1; k <= maxk; k++)
 	{
 		n = pow(2, k);
-		oss << "n = 2^" << k << " = " << n << " M_PI - pi_n = " << abs(M_PI - run_calculation(2, n)) << endl;
+
+		auto answer = abs(M_PI - calculate_pi(rank, 2, n));
+
+		if (rank == 0){
+			oss << "n =" << n << ", error: M_PI - pi_n = " << answer << endl;
+		}
 	}
 
 	return oss.str();
@@ -31,37 +41,61 @@ string verification_test(const int maxk)
 
 int main(int argc, char* argv[])
 {
-	auto n = 3;
+	auto n = 100;
 	auto argument_number = 1;
+	auto numberOfProsesses = 2;
+	int rank;
 
-	cout.precision(numeric_limits<double>::digits10 + 2);
+	MPI_Init(&argc , &argv);
+	MPI_Comm_size(MPI_COMM_WORLD, &numberOfProsesses);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-	if (argc == argument_number)
-	{
-		cout << fixed << "Calculate pi for zeta1 function: " << run_calculation(2, n) << endl;
+	if (rank == 0) {
+		cout.precision(numeric_limits<double>::digits10 + 2);
 	}
+	
+	if (argc == argument_number) {
+		// Parallel calculation
+		auto answer = calculate_pi(rank, 2, n);
 
-	else
-	{
+		// Seriel print:
+		if (rank == 0){
+			cout << fixed << "Calculate pi for zeta1 function: " << answer << endl;
+		}
+	} else {
 		string arg = argv[argument_number];
 		if (arg == "-u")
 		{
-			cout << fixed << "zeta1 unit test result, with n = 3: " << boolalpha << unit_test() << endl;
-		}
-		else if (arg== "-v")
-		{
-			cout << fixed << "Running zeta1 verification tests: " << endl;
-			cout << verification_test(24);
-		}
-		else{
-			int argument = stoi(arg);
-			if((argument & (argument-1)) != 0 && argument != 0){
-				cout << "Number of processes need to be power of two";
-				return -1;
+			// Parallel calculation
+			auto boolalpha = unit_test(rank);
+
+			// Seriel print:
+			if (rank == 0){
+				cout << fixed << "zeta1 unit test result, with n = 3: " << boolalpha << endl;
 			}
-			cout << fixed << "Running zeta1 with " << arg << " processes." << endl;
-			cout << fixed << run_calculation(2, argument) << endl;
+		}
+		else {
+			auto argument = stoi(arg);
+			// Seriel start:
+			if (rank == 0){
+				if ( (argument & (argument-1) ) != 0 && argument != 0) {
+					cout << fixed << "Number of processes need to be power of two";
+					return -1;
+				}
+			}
+
+			// Parallel calculation
+			auto answer = calculate_pi(rank, 2, argument);
+
+			// Seriel print:
+			if (rank == 0){
+				cout << fixed << "Running zeta1 with " << arg << " processes." << endl;
+				cout << fixed << answer << endl;
+			}
 		}
 	}
+
+	MPI_Finalize();
+
 	return 0;
 }
--- P1/src/zeta1/zeta1.cpp
+++ P1/src/zeta1/zeta1.cpp
@@ -13,7 +13,14 @@ void zeta1_calculate_vi(const int &numberOfProcesses, const int &rank, const int
 		answers[j] = v_i;
 		j++;
 	}
-	
+}
+
+void zeta1_calculate_vi(const int &numberOfProcesses, const int &rank, const int &n, double &sumPiParts)
+{
+	for (int i = rank + 1; i <= n; i += numberOfProcesses)
+	{
+		sumPiParts += 1.0 / (i*i);
+	}
 }
 
 void sumVector(const double* vector, const int& length, double& sum){
@@ -22,50 +29,35 @@ void sumVector(const double* vector, const int& length, double& sum){
 	}
 }
 
+double zeta1_calculate_pi(int &rank, const int &numberOfProcessors, const int &numberOfIntervals){
+	int length = numberOfIntervals / numberOfProcessors + (numberOfIntervals % numberOfProcessors == 0 ? 0 : 1);
 
-double run_calculation(const int &numberOfProcessors, const int &numberOfIntervals){
-	int nOP, rank;
-	nOP = numberOfProcessors;
-	int lengthForRank[nOP];
-	int length = numberOfIntervals / nOP;
-	int mod = numberOfIntervals % nOP;
-	
-	for(int i = 0; i < nOP; i++){
-		int temp = length;
-		if(i < mod){
-			temp += 1;
-		}
-		cout << "Length for rank " << i << temp << endl;
-		lengthForRank[i] = temp;
-	}
-	
-	MPI_Init(NULL,NULL);
-	MPI_Comm_size(MPI_COMM_WORLD, &nOP);
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	
 	double startTime;
-	if(rank == 0) startTime = MPI_Wtime();
-
-	double answers[lengthForRank[rank]];
-
-	zeta1_calculate_vi(nOP, rank, numberOfIntervals, answers);
-
-	double myPI = 0.0;
-
-	sumVector(answers, lengthForRank[rank], myPI);
-	
-	double pi;
-	MPI_Reduce(&myPI, &pi, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
 	if(rank == 0){
-		cout << "PI:" << pi << endl; 
-		pi = sqrt(pi * 6);
-	}
+		startTime = MPI_Wtime();
+	} 
 
-	MPI_Finalize();
+	// double piParts[length];
+	// double sumPiParts = 0.0; 
+	
+	// zeta1_calculate_vi(numberOfProcessors, rank, numberOfIntervals, piParts);
+	// sumVector(piParts, length, sumPiParts);
+
+	double sumPiParts = 0.0; 
+	zeta1_calculate_vi(numberOfProcessors, rank, numberOfIntervals, sumPiParts);
+	
+	double pi; 
+	MPI_Reduce(&sumPiParts, &pi, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+
+	if(rank == 0){
+		pi = sqrt(pi * 6);
+
+		double endTime = MPI_Wtime();
+
+		cout << "Main prosess used " << (endTime - startTime) << endl;
+	} else {
+		cout << "Helper prosess " << rank << " done."<< endl;  
+	}
 	
 	return pi;
-	
 }
-
-
-
