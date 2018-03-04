64a8de9560fa6bc638c029bd467d703f03241dda
Sindre Gjerde <sindread@stud.ntnu.no>
2018-03-04 19:59:03 +0100

    send and recive

--- P1/src/zeta1/zeta1.cpp
+++ P1/src/zeta1/zeta1.cpp
@@ -4,60 +4,85 @@
 
 using namespace std;
 
-// void zeta1_calculate_vi(const int &numberOfProcesses, const int &rank, const int &n, double* answers)
-// {
-// 	int j = 0;
-// 	for (int i = rank + 1; i <= n; i += numberOfProcesses)
-// 	{
-// 		const double v_i = 1.0 / (i*i);	
-// 		answers[j] = v_i;
-// 		j++;
-// 	}
-// }
-
-void zeta1_calculate_vi(const int &numberOfProcesses, const int &rank, const int &n, double &sumPiParts)
-{
-	for (int i = rank + 1; i <= n; i += numberOfProcesses)
-	{
-		sumPiParts += 1.0 / (i*i);
+void master_init(int argc, char* argv[], int &n, bool &isUnitTest){
+	if (argc == 2){
+		n = stoi(argv[1]);
+	} else if (argc == 3) {
+		auto arg = argv[2];
+		if (arg == "-u"){
+			isUnitTest = true;
+			n = 3;
+		}
+	} else {
+		n = 4;
 	}
+}
+
+void master_task(const int &n, const int &numberOfProcesses){
+	double* vi = vi_parts(n);
+
+	int lengthForRank[numberOfProcesses];
+	int length = n / (numberOfProcesses-1);
+	int rest = n % (numberOfProcesses-1);
+	
+	for(int i = 0; i < numberOfProcesses-1; i++){
+		lengthForRank[i] = length;
+
+		if(i < rest){
+			lengthForRank[i]++;
+		}
+	}
+
+	int counter = 0;
+
+	for (auto i = 1; i < numberOfProcesses; i++){
+		if (rest > 0){
+			MPI_Send(&lengthForRank[i-1], 1, MPI_INT, i, TAG_LENGTH, MPI_COMM_WORLD);
+			MPI_Send(&vi, lengthForRank[i-1], MPI_DOUBLE, i, TAG_VPARTS, MPI_COMM_WORLD);
+			vi = vi + lengthForRank[i-1];
+		}
+	}
+
+	double sum = 0.0;
+	double part;
+	int sources = 1;
+	while (sources < numberOfProcesses){
+		MPI_Recv(&part, 1, MPI_DOUBLE, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		sum += part;
+		sources++;
+	}
+
+	double pi = sqrt(6*sum);
+
+	cout << pi << endl;
+}
+
+void slave_task(int &rank, int &numberOfProcesses){
+	int length;
+	MPI_Recv(&length, 1, MPI_INT, 0, TAG_LENGTH, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+	
+	double vi_parts[length];
+	MPI_Recv(vi_parts, length, MPI_DOUBLE, 0, TAG_VPARTS, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+	double piPart;
+	sumVector(vi_parts, length, piPart);
+
+	MPI_Send(&piPart, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
+}
+
+double* vi_parts(const int &n)
+{
+	double parts[n];
+	for (int i = 1; i < n; i++)
+	{
+		 parts[i-1] = 1.0 / pow(i,2);
+	}
+
+	return parts;
 }
 
 void sumVector(const double* vector, const int& length, double& sum){
 	for(int i = 0; i < length; i++){
 		sum += vector[i];	
 	}
-}
-
-double zeta1_calculate_pi(const int &rank, const int &numberOfProcessors, const int &numberOfIntervals){
-	int length = numberOfIntervals / numberOfProcessors + (numberOfIntervals % numberOfProcessors == 0 ? 0 : 1);
-
-	double startTime;
-	if(rank == 0){
-		startTime = MPI_Wtime();
-	} 
-
-	// double piParts[length];
-	// double sumPiParts = 0.0; 
-	
-	// zeta1_calculate_vi(numberOfProcessors, rank, numberOfIntervals, piParts);
-	// sumVector(piParts, length, sumPiParts);
-
-	double sumPiParts = 0.0; 
-	zeta1_calculate_vi(numberOfProcessors, rank, numberOfIntervals, sumPiParts);
-	
-	double pi; 
-	MPI_Reduce(&sumPiParts, &pi, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
-
-	if(rank == 0){
-		pi = sqrt(pi * 6);
-
-		double endTime = MPI_Wtime();
-
-		cout << "Main prosess used " << (endTime - startTime) << endl;
-	} else {
-		cout << "Helper prosess " << rank << " done."<< endl;  
-	}
-	
-	return pi;
 }
--- P1/src/zeta1/zeta1.h
+++ P1/src/zeta1/zeta1.h
@@ -1,7 +1,12 @@
 ﻿#pragma once
 
 const double zeta1_expected_value_after_3_iterations = 2.85773803324704145; //114563498087156873290626938727432781816504;
+const int TAG_LENGTH = 42;
+const int TAG_VPARTS = 1337;
 
-double zeta1_calculate_pi(const int &rank, const int &numberOfProcessors, const int &numberOfIntervals);
-void zeta1_calculate_vi(const int &numberOfProcesses, const int &rank, const int &n, double &sumPiParts);
+void master_init(int argc, char* argv[], int &n, bool &isUnitTest);
+void master_task(const int &n, const int &numberOfProcesses);
+void slave_task(int &rank, int &numberOfProcesses);
+double* vi_parts(const int &n);
+void vi_parts(double* parts, const int &length);
 void sumVector(const double* vector, const int& length, double& sum);
