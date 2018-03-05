3765831c8b759b05dd2de43ed3a4572cf46eba16
Sindre Gjerde <sindread@stud.ntnu.no>
2018-03-05 10:06:27 +0100

    check if nOp is of power 2

--- P1/src/mach1/main.cpp
+++ P1/src/mach1/main.cpp
@@ -12,13 +12,16 @@ int main(int argc, char* argv[])
 	MPI_Comm_size(MPI_COMM_WORLD, &numberOfProcesses);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-	if (rank == 0){
-		if ( (numberOfProcesses & (numberOfProcesses-1) ) != 0 && numberOfProcesses != 0) {
-			cout << fixed << "Number of processes need to be power of two";
-			MPI_Finalize();
-			return -1;
+	if ( (numberOfProcesses & (numberOfProcesses-1) ) != 0 && numberOfProcesses != 0) {
+		if (rank = 0){
+			cout << fixed << "Number of processes need to be power of two"<< endl;
 		}
+		
+		MPI_Finalize();
+		return -1;
+	}
 
+	if (rank == 0){
 		int n;
 		master_init(argc, argv, n);
 		master_task(n, numberOfProcesses);
