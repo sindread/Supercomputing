6dd20aeb8fd3ee33e3971f8b79c389ea9abd694b
Stein-Erik Bjørnnes <steinerikbjornnes@gmail.com>
2018-04-24 18:58:23 +0200

    progress

--- P2/poisson.c
+++ P2/poisson.c
@@ -12,6 +12,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+#include <mpi.h>
+#include <omp.h>
 
 #define PI 3.14159265358979323846
 #define true 1
@@ -34,20 +36,46 @@ void fst_(real *v, int *n, real *w, int *nn);
 void fstinv_(real *v, int *n, real *w, int *nn);
 
 
-// Our:
+//Our code
 int* sendcounts; 
 int* sdispls; 
 int* recvcounts; 
 int* rdispls;
+void transpose_parallel(real **bt, real **b, size_t m, int* sendcount, int* senddis, int* recvcount, int* recvdis);
+void divide_work(size_t m, int numProcs, int rank, int* sendr, int* senddis, int* recvr, int* recvdis);
 
 int main(int argc, char **argv)
 {
-    if (argc < 2) {
-        printf("Usage:\n");
-        printf("  poisson n\n\n");
-        printf("Arguments:\n");
-        printf("  n: the problem size (must be a power of 2)\n");
+    int numProcs, rank, numThreads, startTime;
+    int* sendcount, *senddis, *recvcount, *recvdis;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &numProcs);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    numThreads = atoi(argv[2]);
+
+    printf("numprocs %d \n", numProcs);
+    if(rank == 0){
+        if (argc < 3) {
+            printf("Usage:\n");
+            printf("  poisson n\n\n");
+            printf("Arguments:\n");
+            printf("  n: the problem size (must be a power of 2)\n");
+            printf("  t: number of threads\n");
+
+            MPI_Finalize();
+            return -1;
+        }
+        
+        printf("Running with %d processes and %d threads\n", numProcs, numThreads);
+        startTime = MPI_Wtime();
     }
+    
+    
+    omp_set_dynamic(0);
+    omp_set_num_threads(numThreads);
+    
 
     /*
      *  The equation is solved on a 2D structured grid and homogeneous Dirichlet
@@ -60,10 +88,26 @@ int main(int argc, char **argv)
     int m = n - 1;
     real h = 1.0 / n;
 
+    if((n & (n-1))!= 0 && n){
+        if(rank == 0){
+            printf("Problem size needs to be power of 2\n");
+        }
+
+        MPI_Finalize();
+        return -1;
+    }
+    sendcount = (int*) malloc(numProcs*sizeof(int));
+    recvcount = (int*) calloc(numProcs,sizeof(int));
+    senddis = (int*) malloc(numProcs*sizeof(int));
+    recvdis = (int*) calloc(numProcs,sizeof(int));
+
+    divide_work(m, numProcs, rank, sendcount, senddis, recvcount, recvdis);
+
     /*
      * Grid points are generated with constant mesh size on both x- and y-axis.
      */
     real *grid = mk_1D_array(n+1, false);
+    #pragma omp parallel for 
     for (size_t i = 0; i < n+1; i++) {
         grid[i] = i * h;
     }
@@ -74,6 +118,7 @@ int main(int argc, char **argv)
      * Note that the indexing starts from zero here, thus i+1.
      */
     real *diag = mk_1D_array(m, false);
+    #pragma omp parallel for 
     for (size_t i = 0; i < m; i++) {
         diag[i] = 2.0 * (1.0 - cos((i+1) * PI / n));
     }
@@ -107,6 +152,7 @@ int main(int argc, char **argv)
      * of freedom, so it excludes the boundary (bug fixed by petterjf 2017).
      * 
      */
+    #pragma omp parallel for  collapse(2)
     for (size_t i = 0; i < m; i++) {
         for (size_t j = 0; j < m; j++) {
             b[i][j] = h * h * rhs(grid[i+1], grid[j+1]);
@@ -123,10 +169,13 @@ int main(int argc, char **argv)
      * In functions fst_ and fst_inv_ coefficients are written back to the input 
      * array (first argument) so that the initial values are overwritten.
      */
+    #pragma omp parallel for 
     for (size_t i = 0; i < m; i++) {
         fst_(b[i], &n, z, &nn);
     }
-    transpose(bt, b, m);
+    //transpose(bt, b, m); Need to implement transpose for parallel
+    transpose_parallel(bt, b, m, sendcount, senddis, recvcount, recvdis);
+    #pragma omp parallel for 
     for (size_t i = 0; i < m; i++) {
         fstinv_(bt[i], &n, z, &nn);
     }
@@ -134,6 +183,7 @@ int main(int argc, char **argv)
     /*
      * Solve Lambda * \tilde U = \tilde G (Chapter 9. page 101 step 2)
      */
+    #pragma omp parallel for 
     for (size_t i = 0; i < m; i++) {
         for (size_t j = 0; j < m; j++) {
             bt[i][j] = bt[i][j] / (diag[i] + diag[j]);
@@ -143,10 +193,14 @@ int main(int argc, char **argv)
     /*
      * Compute U = S^-1 * (S * Utilde^T) (Chapter 9. page 101 step 3)
      */
+    #pragma omp parallel for 
     for (size_t i = 0; i < m; i++) {
         fst_(bt[i], &n, z, &nn);
     }
-    transpose(b, bt, m);
+    //transpose(b, bt, m);
+    transpose_parallel(bt, b, m, sendcount, senddis, recvcount, recvdis);
+    
+    #pragma omp parallel for 
     for (size_t i = 0; i < m; i++) {
         fstinv_(b[i], &n, z, &nn);
     }
@@ -156,16 +210,17 @@ int main(int argc, char **argv)
      * norm.
      */
     double u_max = 0.0;
+    #pragma omp parallel for  collapse(2)
     for (size_t i = 0; i < m; i++) {
         for (size_t j = 0; j < m; j++) {
             u_max = u_max > b[i][j] ? u_max : b[i][j];
         }
     }
-
-    printf("u_max = %e\n", u_max);
-
-    printMatrix(b, m);
-
+    
+    if(rank == 0){
+        printf("u_max = %e\n", u_max);    
+        printMatrix(b,m);
+    }
     return 0;
 }
 
@@ -183,7 +238,8 @@ void printMatrix(real** matrix, int length){
  * Other functions can be defined to swtich between problem definitions.
  */
 
-real rhs(real x, real y) {
+real rhs(real x, real y) 
+{
     return 2 * (y - y*y + x - x*x);
 }
 
